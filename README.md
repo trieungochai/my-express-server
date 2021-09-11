c6c5f80f6dee7b2651ff5953ce9ae97d149d39d4
Cai <trieungochai.dev@gmail.com>
2021-09-11 15:28:09 +0700

    Understanding and Working with Routes

---
+++ README.md
@@ -0,0 +1,8 @@
+## Nodemon Installation
+
+a npm package that will make it way easier for us to auto start our servers.
+
+### Problems with Installing nodemon
+
+- So when this command doesn't work: `npm install -g nodemon`
+- I recommend to use this one instead: `sudo npm install -g nodemon`
--- server.js
+++ server.js
@@ -5,6 +5,18 @@ app.get("/", function (req, res) {
   res.send("<h1>See you Space Cowboy</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("Contact me at: trieungochai.dev@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send("I'm Hai and I love drinking and coding!!");
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<ul><li>Tea</li><li>Beer</li></ul>");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
